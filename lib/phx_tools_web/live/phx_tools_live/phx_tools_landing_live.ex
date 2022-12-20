bb0dc7dd6522109910be530cd57756855fa66921
Senaid Karauzovic <59146048+senaid123@users.noreply.github.com>
2022-12-20 11:42:54 +0100

    Update lib/phx_tools_web/live/phx_tools_live/phx_tools_landing_live.ex
    Co-authored-by: Almir Sarajčić <almir@almirsarajcic.com>

--- lib/phx_tools_web/live/phx_tools_live/phx_tools_landing_live.ex
+++ lib/phx_tools_web/live/phx_tools_live/phx_tools_landing_live.ex
@@ -2,15 +2,7 @@ defmodule PhxToolsWeb.LandingPageLive.Index do
   use PhxToolsWeb, :live_view
 
   @impl Phoenix.LiveView
-  def mount(_params, session, socket) do
-    socket = assign_os_param(socket, session)
+  def mount(_params, session, socket), do: {:ok, assign_os_param(socket, session)}
 
-    {:ok, socket}
-  end
-
-  defp assign_os_param(socket, %{"operating_system" => operating_system} = _session) do
-    socket = assign(socket, :operating_system, operating_system)
-
-    socket
-  end
+  defp assign_os_param(socket, %{"operating_system" => operating_system} = _session), do: assign(socket, :operating_system, operating_system)
 end
