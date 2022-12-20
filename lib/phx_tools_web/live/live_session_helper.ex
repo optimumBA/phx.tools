9b951c13641350ec30832a1ed8cabe22cd32fae2
Senaid Karauzovic <59146048+senaid123@users.noreply.github.com>
2022-12-20 11:28:18 +0100

    Update lib/phx_tools_web/live/live_session_helper.ex
    Co-authored-by: Almir Sarajčić <almir@almirsarajcic.com>

--- lib/phx_tools_web/live/live_session_helper.ex
+++ lib/phx_tools_web/live/live_session_helper.ex
@@ -5,8 +5,8 @@ defmodule PhxToolsWeb.LiveSessionHelper do
   """
   alias Plug.Conn
 
-  @spec get_os_version(Plug.Conn.t()) :: map()
-  def get_os_version(conn) do
+  @spec get_system_name(Plug.Conn.t()) :: map()
+  def get_system_name(conn) do
     operating_system = Conn.get_session(conn, :operating_system)
 
     case get_os_name(operating_system.os.family) do
