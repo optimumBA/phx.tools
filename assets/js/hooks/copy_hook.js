74080eed15fd902052e6328efd93d575e05bcb49
senaid123 <senaid968@gmail.com>
2022-12-20 23:16:14 +0100

    refactor copy_hook

--- assets/js/hooks/copy_hook.js
+++ assets/js/hooks/copy_hook.js
@@ -3,9 +3,10 @@ export default {
     this.el.addEventListener('click', () => {
       var copyText = document.getElementById('tool-installation').innerHTML
       navigator.clipboard.writeText(copyText)
-      document.getElementById('copy').innerHTML = 'Copied'
-      document.getElementById('copy').setAttribute('disabled', '')
-      document.getElementById('copy').classList.add('bg-[#9887FF]')
+      copyBtn = document.getElementById('copy')
+      copyBtn.innerHTML = 'Copied'
+      copyBtn.setAttribute('disabled', '')
+      copyBtn.classList.add('bg-[#9887FF]')
     })
   },
 }
