7b6bb375356808f4e7419d9182a28b1ce6953182
Nittin Shankar <nittin.shankar@gmail.com>
2023-01-31 06:09:10 +0530

    Use one component with several slots instead of two components

--- lib/phx_tools_web/live/phx_tools_live/instruction_components.ex
+++ lib/phx_tools_web/live/phx_tools_live/instruction_components.ex
@@ -3,24 +3,28 @@ defmodule PhxToolsWeb.InstructionComponents do
   Shows the steps to execute different scripts depending on the operating system.
   """
   use PhxToolsWeb, :html
+  use Phoenix.Component
 
-  @spec linux(map()) :: Phoenix.LiveView.Rendered.t()
-  def linux(assigns) do
+  slot :header, required: true
+  slot :command_to_install, required: true
+  slot :instruction_item, required: true
+
+  def os_instructions(assigns) do
     ~H"""
     <div class="solved-height font-normal">
       <div class="w-full h-full flex items-center justify-center">
         <div class="block lg:mt-[3%] ">
           <h1 class="text-white text-center lg:text-[24px] md:text-[22px] sm:text-[20px] lg:mb-[5%] md:mb-[2%] sm:mb-[2%]">
-            Linux installation process
+            <%= render_slot(@header) %>
           </h1>
-          <div class="lg:w-[671px] lg:h-[272px] md:w-[661px] md:h-[290px] sm:w-[330px] sm:h-[350px] border-[4px] shadow-[#28177E] shadow-md border-[#211666] rounded-md lg:mx-[2%]">
+          <div class="h-full w-full border-[4px] shadow-[#28177E] shadow-md border-[#211666] rounded-md lg:mx-[2%] pb-2">
             <div class="text-start px-[3%] lg:text-[20px] md:text-[18px] sm:text-[15px]">
               <h1 class="text-white lg:mt-5 md:mt-5 sm:mt-4">
                 1. Press the button to copy this command to your clipboard:
               </h1>
               <div class="flex items-center justify-evenly border-[1px] border-[#755FFF] py-1 mx-[4%] lg:mt-6 md:mt-5 sm:mt-4 rounded-lg">
                 <h1 id="tool-installation" class="text-white  text-[16px] font-thin">
-                  /bin/bash -c "$(curl -fsSL <%= @url %>/Linux.sh)"
+                  <%= render_slot(@command_to_install) %>
                 </h1>
                 <button
                   id="copy"
@@ -30,66 +34,12 @@ defmodule PhxToolsWeb.InstructionComponents do
                   Copy
                 </button>
               </div>
-              <h1 class="text-white lg:mt-4 md:mt-5 sm:mt-4">
-                2. Open Terminal by pressing <b class="font-extrabold">Ctrl + Alt + T</b> together
-              </h1>
-              <h1 class="text-white lg:mt-3 md:mt-5 sm:mt-4">
-                3. Paste the shell command by hitting <b>Ctrl + Alt + V</b> together
-              </h1>
-              <h1 class="text-white lg:mt-3 md:mt-5 sm:mt-4">
-                4. Run the command by hitting <b>ENTER</b>
-              </h1>
-            </div>
-          </div>
-          <.link navigate={~p"/"}>
-            <button class="border-[#755FFF] border-[1px] lg:mt-8 md:mt-5 sm:mt-5 p-1 text-[#755FFF] text-[16px] rounded-md lg:ml-3">
-              Back
-            </button>
-          </.link>
-        </div>
-      </div>
-    </div>
-    """
-  end
 
-  @spec mac_os(map()) :: Phoenix.LiveView.Rendered.t()
-  def mac_os(assigns) do
-    ~H"""
-    <div class="solved-height font-normal">
-      <div class="w-full h-full flex items-center justify-center">
-        <div class="block lg:mt-[3%]">
-          <h1 class="text-white text-center lg:text-[24px] md:text-[22px] sm:text-[20px] lg:mb-[5%] md:mb-[2%] sm:mb-[2%]">
-            macOS installation process
-          </h1>
-          <div class="lg:w-[683px] lg:h-[304px] md:w-[683px] md:h-[300px] sm:w-[330px] sm:h-[400px] border-[4px] shadow-[#28177E] shadow-md border-[#211666] rounded-md lg:mx-[2%]">
-            <div class="text-start px-[3%] lg:text-[20px] md:text-[18px] sm:text-[15px]">
-              <h1 class="text-white lg:mt-4 md:mt-5 sm:mt-4">
-                1. Press the button to copy this command to your clipboard:
-              </h1>
-              <div class="flex items-center justify-evenly border-[1px] border-[#755FFF] py-1 mx-[4%] lg:mt-6 md:mt-5 sm:mt-4 rounded-lg">
-                <h1 id="tool-installation" class="text-white text-center text-[16px] font-thin">
-                  /bin/bash -c "$(curl -fsSL <%= @url %>/macOS.sh)"
+              <%= for item <- @instruction_item do %>
+                <h1 class="text-white lg:mt-4 md:mt-5 sm:mt-4">
+                  <%= render_slot(item) %>
                 </h1>
-                <button
-                  id="copy"
-                  phx-hook="CopyHook"
-                  class="text-white bg-[#755FFF] py-1 px-2 rounded-lg lg:text-[16px] md:text-[15px] sm:text-[14px]"
-                >
-                  Copy
-                </button>
-              </div>
-              <h1 class="text-white lg:mt-4 md:mt-4 sm:mt-4">
-                2. Open Terminal by pressing <b class="font-extrabold"> ⌘ + SPACE </b> together
-              </h1>
-              <h1 class="text-white lg:mt-3 md:mt-4 sm:mt-4">
-                3. Type "Terminal" and hit <b>RETURN</b>
-              </h1>
-              <h1 class="text-white lg:mt-3 md:mt-4 sm:mt-4">
-                4. Paste the shell command by hitting<b> ⌘ + V </b> together.
-              </h1>
-              <h1 class="text-white lg:mt-3 md:mt-4 sm:mt-4">
-                5. Run the command by hitting <b>RETURN</b>
-              </h1>
+              <% end %>
             </div>
           </div>
           <.link navigate={~p"/"}>
