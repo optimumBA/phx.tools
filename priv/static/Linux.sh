fac3551b7351aa6e8c9813a229f2d5763e342411
Almir Sarajčić <almir.sarajcic@icloud.com>
2023-02-13 11:25:09 +0100

    Fix script execution inside Docker container

--- priv/static/Linux.sh
+++ priv/static/Linux.sh
@@ -1,5 +1,17 @@
 #! /bin/bash
 
+# Make sure important variables exist if not already defined
+#
+# $USER is defined by login(1) which is not always executed (e.g. containers)
+# POSIX: https://pubs.opengroup.org/onlinepubs/009695299/utilities/id.html
+USER=${USER:-$(id -u -n)}
+# $HOME is defined at the time of login, but it could be unset. If it is unset,
+# a tilde by itself (~) will not be expanded to the current user's home directory.
+# POSIX: https://pubs.opengroup.org/onlinepubs/009696899/basedefs/xbd_chap08.html#tag_08_03
+HOME="${HOME:-$(getent passwd $USER 2>/dev/null | cut -d: -f6)}"
+# macOS does not have getent, but this works even if $HOME is unset
+HOME="${HOME:-$(eval echo ~$USER)}"
+
 bold=$(tput bold)
 normal=$(tput sgr0)
 red='\033[0;31m'
