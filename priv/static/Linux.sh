d3929d06abb2e7bca869c8bcb692908b76baf1a4
Almir Sarajčić <almir.sarajcic@icloud.com>
2023-02-15 09:48:29 +0100

    Make asdf installations consistent

--- priv/static/Linux.sh
+++ priv/static/Linux.sh
@@ -98,7 +98,9 @@ function install() {
         sudo apt-get update
         sudo apt-get -y install build-essential autoconf m4 libncurses5-dev libwxgtk3.0-gtk3-dev libwxgtk-webview3.0-gtk3-dev libgl1-mesa-dev libglu1-mesa-dev libpng-dev libssh-dev unixodbc-dev xsltproc fop libxml2-utils libncurses-dev openjdk-11-jdk
         asdf plugin add erlang https://github.com/asdf-vm/asdf-erlang.git
-        asdf install erlang 25.1.2 && asdf global erlang 25.1.2
+        asdf install erlang 25.1.2
+        asdf global erlang 25.1.2
+        asdf reshim erlang 25.1.2
         ;;
     "Elixir")
         asdf plugin add elixir https://github.com/asdf-vm/asdf-elixir.git
