73f573efa52b51a2843ea5ac9ab3167fae94e2ca
Mir <37258290+amir-repository@users.noreply.github.com>
2023-02-21 17:42:13 +0700

    Shell scripts for Linux (#5)
    Co-authored-by: Almir Sarajčić <almir.sarajcic@icloud.com>

--- .github/github_workflows.ex
+++ .github/github_workflows.ex
@@ -342,7 +342,7 @@ defmodule GitHubWorkflows do
         [
           name: "Generate an app and start the server",
           if: "steps.result_cache.outputs.cache-hit != 'true'",
-          run: "make -f test/scripts/Makefile"
+          run: "/bin/zsh -c 'source ~/.zshrc && make -f test/scripts/Makefile'"
         ],
         [
           name: "Check HTTP status code",
