aa1d179c54bf92c8ece7ab7c1be8a2e30b670c1e
Almir Sarajčić <almir.sarajcic@icloud.com>
2023-02-25 09:54:33 +0100

    Fix Not Found error for the scripts

--- test/phx_tools_web/live/index_test.exs
+++ test/phx_tools_web/live/index_test.exs
@@ -4,7 +4,7 @@ defmodule PhxToolsWeb.PhxToolsLive.IndexTest do
   import Phoenix.LiveViewTest
 
   describe "landing page" do
-    test "background of macOS card changes when Mac OS user visits the page", %{conn: conn} do
+    test "background of macOS card changes when macOS user visits the page", %{conn: conn} do
       conn = put_req_header(conn, "user-agent", "Mac OS X 10_5_7")
 
       {:ok, landing_live, html} = live(conn, "/")
@@ -30,8 +30,8 @@ defmodule PhxToolsWeb.PhxToolsLive.IndexTest do
     end
   end
 
-  describe "linux instructions page" do
-    test "user visits linux instructions page", %{conn: conn} do
+  describe "Linux instructions page" do
+    test "user visits Linux instructions page", %{conn: conn} do
       {:ok, linux_live, html} = live(conn, "/linux")
 
       assert html =~ "Linux installation process"
@@ -41,14 +41,26 @@ defmodule PhxToolsWeb.PhxToolsLive.IndexTest do
     end
   end
 
-  describe "macos instructions page" do
-    test "user visits macos instructions page", %{conn: conn} do
+  describe "macOS instructions page" do
+    test "user visits macOS instructions page", %{conn: conn} do
       {:ok, linux_live, html} = live(conn, "/macOS")
 
       assert html =~ "macOS installation process"
       assert render(linux_live) =~ "macOS installation process"
       assert has_element?(linux_live, "#tool-installation")
       assert has_element?(linux_live, "#copy")
+    end
+  end
+
+  describe "scripts" do
+    test "user accesses Linux script", %{conn: conn} do
+      conn = get(conn, "/Linux.sh")
+      assert conn.resp_body =~ "Welcome to the phx.tools shell script for Linux-based OS."
+    end
+
+    test "user accesses macOS script", %{conn: conn} do
+      conn = get(conn, "/macOS.sh")
+      assert conn.resp_body =~ "Welcome to the phx.tools shell script for macOS."
     end
   end
 end
