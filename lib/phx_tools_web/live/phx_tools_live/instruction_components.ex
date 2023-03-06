b7c44918e4cd8e70184c596306efa1f50b8defdf
amos-kibet <kibetamos97@gmail.com>
2023-03-06 12:28:57 +0300

    add hover border to improve UX

--- lib/phx_tools_web/live/phx_tools_live/instruction_components.ex
+++ lib/phx_tools_web/live/phx_tools_live/instruction_components.ex
@@ -20,13 +20,13 @@ defmodule PhxToolsWeb.InstructionComponents do
           <h1 class="text-white text-center lg:text-2xl md:text-[22px] sm:text-xl lg:mb-[5%] md:mb-[2%] sm:mb-[2%]">
             <%= render_slot(@header) %>
           </h1>
-          <div class="h-full w-full border-4 shadow-[#28177E] shadow-md border-[#211666] rounded-md lg:mx-[2%] pb-2">
+          <div class="h-full w-full border-4 hover:border-2 shadow-[#28177E] shadow-md border-[#211666] hover:border-indigo-450 rounded-md lg:mx-[2%] pb-2">
             <div class="text-start px-[3%] lg:text-xl md:text-lg sm:text-md">
               <h1 class="text-white lg:mt-5 md:mt-5 sm:mt-4">
                 1. Press the button to copy this command to your clipboard:
               </h1>
               <div class="flex items-center justify-evenly border border-indigo-450 py-1 mx-[4%] lg:mt-6 md:mt-5 sm:mt-4 rounded-lg">
-                <h1 id="tool-installation" class="text-white  text-base font-thin">
+                <h1 id="tool-installation" class="text-white text-base font-thin">
                   <%= render_slot(@installation_command) %>
                 </h1>
                 <button
