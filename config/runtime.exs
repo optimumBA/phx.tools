232438c006c06039f664cf450c5fb705a9193bf4
amos-kibet <kibetamos97@gmail.com>
2023-03-16 23:52:59 +0300

    debug preview deployment workflow file

--- config/runtime.exs
+++ config/runtime.exs
@@ -20,42 +20,42 @@ if System.get_env("PHX_SERVER") do
   config :phx_tools, PhxToolsWeb.Endpoint, server: true
 end
 
-if config_env() == :prod do
-  appsignal_app_env =
-    System.get_env("APPSIGNAL_APP_ENV") ||
-      raise """
-      environment variable APPSIGNAL_APP_ENV is missing.
-      """
+# if config_env() == :prod do
+#   appsignal_app_env =
+#     System.get_env("APPSIGNAL_APP_ENV") ||
+#       raise """
+#       environment variable APPSIGNAL_APP_ENV is missing.
+#       """
 
-  appsignal_push_api_key =
-    System.get_env("APPSIGNAL_PUSH_API_KEY") ||
-      raise """
-      environment variable APPSIGNAL_PUSH_API_KEY is missing.
-      """
+  # appsignal_push_api_key =
+  #   System.get_env("APPSIGNAL_PUSH_API_KEY") ||
+  #     raise """
+  #     environment variable APPSIGNAL_PUSH_API_KEY is missing.
+  #     """
 
-  revision_file = Path.join([:code.priv_dir(:phx_tools), "REVISION"])
+  # revision_file = Path.join([:code.priv_dir(:phx_tools), "REVISION"])
 
-  appsignal_revision =
-    revision_file
-    |> File.read!()
-    |> String.trim()
+  # appsignal_revision =
+  #   revision_file
+  #   |> File.read!()
+  #   |> String.trim()
 
-  config :appsignal, :config,
-    env: appsignal_app_env,
-    push_api_key: appsignal_push_api_key,
-    revision: appsignal_revision
+  # config :appsignal, :config,
+  #   env: appsignal_app_env,
+  #   push_api_key: appsignal_push_api_key,
+  #   revision: appsignal_revision
 
   # The secret key base is used to sign/encrypt cookies and other secrets.
   # A default value is used in config/dev.exs and config/test.exs but you
   # want to use a different value for prod and you most likely don't want
   # to check this value into version control, so we use an environment
   # variable instead.
-  secret_key_base =
-    System.get_env("SECRET_KEY_BASE") ||
-      raise """
-      environment variable SECRET_KEY_BASE is missing.
-      You can generate one by calling: mix phx.gen.secret
-      """
+  # secret_key_base =
+  #   System.get_env("SECRET_KEY_BASE") ||
+  #     raise """
+  #     environment variable SECRET_KEY_BASE is missing.
+  #     You can generate one by calling: mix phx.gen.secret
+  #     """
 
   host = System.get_env("PHX_HOST") || "example.com"
   port = String.to_integer(System.get_env("PORT") || "4000")
@@ -70,7 +70,7 @@ if config_env() == :prod do
       ip: {0, 0, 0, 0, 0, 0, 0, 0},
       port: port
     ],
-    secret_key_base: secret_key_base
+    # secret_key_base: secret_key_base
 
   # ## SSL Support
   #
