ce0386f4b1b001d9f6f1c194e8761b382c82b52a
ngumokenneth <ngumokenneth97@gmail.com>
2024-08-09 21:30:00 +0300

    Update UI

--- lib/phx_tools_web/live/phx_tools_live/instruction_components.ex
+++ lib/phx_tools_web/live/phx_tools_live/instruction_components.ex
@@ -14,30 +14,14 @@ defmodule PhxToolsWeb.InstructionComponents do
   @spec os_instructions(assigns()) :: rendered()
   def os_instructions(assigns) do
     ~H"""
-    <div class="solved-height font-normal">
-      <div class="w-full h-full flex items-center justify-center">
+    <div class="font-normal solved-height">
+      <div class="flex items-center justify-center w-full h-full">
         <div class="block lg:mt-[3%] ">
           <h1 class="text-white text-center lg:text-2xl md:text-[22px] sm:text-xl lg:mb-[5%] md:mb-[2%] sm:mb-[2%]">
             <%= render_slot(@header) %>
           </h1>
           <div class="h-full w-full border-4 shadow-[#28177E] shadow-md border-[#211666] rounded-md lg:mx-[2%] pb-2">
             <div class="text-start px-[3%] lg:text-xl md:text-lg sm:text-md">
-              <h1 class="text-white lg:mt-5 md:mt-5 sm:mt-4">
-                1. Press the button to copy this command to your clipboard:
-              </h1>
-              <div class="flex items-center justify-evenly border border-indigo-450 p-2 mx-[4%] lg:mt-6 md:mt-5 sm:mt-4 rounded-lg">
-                <h1 id="tool-installation" class="text-white text-base font-thin w-full">
-                  <%= render_slot(@installation_command) %>
-                </h1>
-                <button
-                  id="copy"
-                  phx-hook="CopyHook"
-                  class="text-white bg-indigo-450 py-1 px-2 rounded-lg text-base lg:text-base md:text-md sm:text-sm"
-                >
-                  Copy
-                </button>
-              </div>
-
               <%= for item <- @instruction do %>
                 <h1 class="text-white lg:mt-4 md:mt-5 sm:mt-4">
                   <%= render_slot(item) %>
@@ -45,11 +29,6 @@ defmodule PhxToolsWeb.InstructionComponents do
               <% end %>
             </div>
           </div>
-          <.link patch={~p"/"}>
-            <button class="border-indigo-450 border lg:mt-8 md:mt-5 sm:mt-5 p-1 text-indigo-450 text-base rounded-[23px] px-[16px] py-[8px] lg:ml-3">
-              Back
-            </button>
-          </.link>
         </div>
       </div>
     </div>
--- lib/phx_tools_web/live/phx_tools_live/landing_component.ex
+++ lib/phx_tools_web/live/phx_tools_live/landing_component.ex
@@ -2,45 +2,82 @@ defmodule PhxToolsWeb.PhxToolsLive.LandingComponent do
   @moduledoc """
   Shows the landing page
   """
+
   use PhxToolsWeb, :html
+
+  slot :installation_command, required: true
 
   @spec landing_page(map()) :: Phoenix.LiveView.Rendered.t()
   def landing_page(assigns) do
     ~H"""
     <div class="solved-height">
-      <div class="min-w-full min-h-full flex items-center justify-center">
+      <div class="flex items-center justify-center min-w-full min-h-full">
         <div class="block mt-[5%]">
-          <h1 class="text-white text-center lg:text-2xl md:text-xl sm:text-lg mb-[10%]">
-            Choose your operating system
-          </h1>
-          <div class="flex justify-between lg:px-4 md:px-8 sm:px-10">
-            <.os_link_card
-              id="macOS"
-              href={~p"/macOS"}
-              os_icon={~p"/images/macos.png"}
-              os_name="macOS"
-              class={[
-                "px-3",
-                @operating_system == "Mac" && "bg-indigo-850"
-              ]}
-            />
-            <.os_link_card
-              id="linux"
-              href={~p"/linux"}
-              os_icon={~p"/images/linux.png"}
-              os_name="Linux"
-              class={[
-                "px-1",
-                @operating_system == "Linux" && "bg-indigo-850"
-              ]}
-            />
+          <div>
+            <h1 class="font-extrabold text-center text-white lg:text-7xl md:text-xl sm:text-4xl font-martian bg-slate-500">
+              Phx.tools
+            </h1>
+            <p class="py-5 text-center text-white">
+              The Complete Development Environment for Elixir and Phoenix
+            </p>
+          </div>
+          <div class="items-center justify-center md:flex sm:py-2">
+            <div class="flex justify-end sm:py-4 md:order-last md:py-0">
+              <img src="/images/info_icon.svg" alt="installation command info" class="md:ml-2" />
+            </div>
+            <div class="flex items-center py-2 border rounded-md justify-evenly sm:px-3">
+              <h1 id="tool-installation" class="font-thin text-white ">
+                <%= render_slot(@installation_command) %>
+              </h1>
+              <div id="copy" phx-hook="CopyHook" class="sm:pl-2 sm:my-5">
+                <img src="/images/clipboard.svg" alt="copy icon" />
+              </div>
+            </div>
           </div>
 
-          <%= if @operating_system do %>
+          <div class="flex justify-between my-4">
+            <div class="grid w-full grid-cols-2 gap-3 text-white">
+              <.os_link_card
+                id="macOS"
+                href={~p"/macOS"}
+                os_icon={~p"/images/macos.png"}
+                os_name="macOS"
+                class={[
+                  "px-3 flex justify-center",
+                  @operating_system == "Mac" && "bg-indigo-850"
+                ]}
+              />
+
+              <div class="border-[2px] border-[#755FFF] rounded-xl cursor-pointer hover:bg-indigo-850 flex h-12 font-martian">
+                <button class="flex items-center justify-center w-full space-x-2 text-sm rounded-xl">
+                  <span><img src="/images/Icon.svg" alt="" /></span> <span>Source code</span>
+                </button>
+              </div>
+            </div>
+            <div :if={@operating_system == Linux}>
+              <.os_link_card
+                id="linux"
+                href={~p"/linux"}
+                os_icon={~p"/images/linux.png"}
+                os_name="Linux"
+                class={[
+                  "px-1",
+                  @operating_system == "Linux" && "bg-indigo-850"
+                ]}
+              />
+            </div>
+          </div>
+          <div>
+            <p class="text-center text-white font-martian sm:text-sm">
+              Phx.tools is a shell script for Linux and macOS that configures the development environment for you in a few easy steps. Once you finish running the script, you'll be able to start the database server, create a new Phoenix application, and launch the server.
+            </p>
+          </div>
+
+          <%!-- <%= if @operating_system do %>
             <h1 id="confirmation" class="text-center text-white text-xl mt-[20%]">
               Confirm your choice by clicking
             </h1>
-          <% end %>
+          <% end %> --%>
         </div>
       </div>
     </div>
