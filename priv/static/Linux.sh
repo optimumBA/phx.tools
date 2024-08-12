c93deb4ce319b2daff681f687c775085f338120d
amos-kibet <kibetamos97@gmail.com>
2024-08-13 00:21:27 +0300

    Update installation scripts.
    This includes automatic detection of user's shell, installation of newer versions of existing software, removing unnecessary software, switching from asdf to mise, bug-fixing, etc.

--- priv/static/Linux.sh
+++ priv/static/Linux.sh
@@ -1,16 +1,6 @@
 #! /bin/bash
 
 # Make sure important variables exist if not already defined
-#
-# $USER is defined by login(1) which is not always executed (e.g. containers)
-# POSIX: https://pubs.opengroup.org/onlinepubs/009695299/utilities/id.html
-USER=${USER:-$(id -u -n)}
-# $HOME is defined at the time of login, but it could be unset. If it is unset,
-# a tilde by itself (~) will not be expanded to the current user's home directory.
-# POSIX: https://pubs.opengroup.org/onlinepubs/009696899/basedefs/xbd_chap08.html#tag_08_03
-HOME="${HOME:-$(getent passwd $USER 2>/dev/null | cut -d: -f6)}"
-# macOS does not have getent, but this works even if $HOME is unset
-HOME="${HOME:-$(eval echo ~$USER)}"
 
 bold=$(tput bold)
 normal=$(tput sgr0)
@@ -20,26 +10,25 @@ bblue='\033[1;34m'
 white='\033[0;37m'
 green='\033[0;32m'
 cyan='\033[0;36m'
+current_shell=$(basename "$SHELL")
+
+if [[ $current_shell == "zsh" ]]; then
+    config_file="~/.zshrc"
+else
+    current_shell="bash"
+    config_file="~/.bashrc"
+fi
 
 function already_installed() {
     case $1 in
     "Git")
         which git >/dev/null 2>&1
         ;;
-    "Zsh")
-        dpkg -l | grep -q zsh
-        ;;
-    "oh-my-zsh")
-        [ -d ~/.oh-my-zsh ]
-        ;;
     "wget")
         dpkg -l | grep -q wget
         ;;
-    "Homebrew")
-        which brew >/dev/null 2>&1
-        ;;
-    "asdf")
-        brew list | grep -q asdf
+    "mise")
+        which mise >/dev/null 2>&1
         ;;
     "Erlang")
         command -v erl >/dev/null 2>&1
@@ -51,19 +40,7 @@ function already_installed() {
         mix phx.new --version >/dev/null 2>&1
         ;;
     "PostgreSQL")
-        which pg_ctl >/dev/null 2>&1
-        ;;
-    "Chrome")
-        dpkg -l | grep -q google-chrome-stable
-        ;;
-    "Node.js")
-        which node >/dev/null 2>&1
-        ;;
-    "ChromeDriver")
-        npm list -g | grep -q chromedriver
-        ;;
-    "Docker")
-        which docker >/dev/null 2>&1
+        which psql >/dev/null 2>&1
         ;;
     *)
         echo "Invalid name argument on checking"
@@ -76,91 +53,29 @@ function install() {
     "Git")
         sudo apt-get install -y git
         ;;
-    "Zsh")
-        sudo apt-get install -y zsh
-        ;;
-    "oh-my-zsh")
-        sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-        ;;
     "wget")
         sudo apt-get install -y wget
         ;;
-    "Homebrew")
-        NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-        echo '# Set PATH, MANPATH, etc., for Homebrew.' >>~/.zshrc
-        (
-            echo
-            echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"'
-        ) >>~/.zshrc
-        eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-        ;;
-    "asdf")
-        brew install asdf
-        (
-            echo
-            echo '. $(brew --prefix asdf)/libexec/asdf.sh'
-        ) >>~/.zshrc
-        source ~/.zshrc >/dev/null 2>&1
+    "mise")
+        curl https://mise.run | sh
+        echo 'eval "$(~/.local/bin/mise activate $current_shell)"' >>~/"$config_file"
+        eval "$(~/.local/bin/mise activate $current_shell --shims)"
         ;;
     "Erlang")
-        sudo apt-get update
-        sudo apt-get -y install build-essential autoconf m4 libncurses5-dev libwxgtk3.0-gtk3-dev libwxgtk-webview3.0-gtk3-dev libgl1-mesa-dev libglu1-mesa-dev libpng-dev libssh-dev unixodbc-dev xsltproc fop libxml2-utils libncurses-dev openjdk-11-jdk
-        asdf plugin add erlang https://github.com/asdf-vm/asdf-erlang.git
-        asdf install erlang 27.0
-        asdf global erlang 27.0
-        asdf reshim erlang 27.0
+        mise use -g erlang@27.0.1
         ;;
     "Elixir")
-        asdf plugin add elixir https://github.com/asdf-vm/asdf-elixir.git
-        asdf install elixir 1.17.1-otp-27
-        asdf global elixir 1.17.1-otp-27
-        asdf reshim elixir 1.17.1-otp-27
+        mise use -g elixir@1.17.2-otp-27
         ;;
     "Phoenix")
-        source ~/.zshrc >/dev/null 2>&1
         mix local.hex --force
-        mix archive.install --force hex phx_new 1.7.0-rc.3
+        mix archive.install --force hex phx_new 1.7.14
         ;;
     "PostgreSQL")
         sudo apt-get update
         sudo apt-get -y install linux-headers-generic build-essential libssl-dev libreadline-dev zlib1g-dev libcurl4-openssl-dev uuid-dev icu-devtools
 
-        asdf plugin add postgres https://github.com/smashedtoatoms/asdf-postgres.git
-        asdf install postgres 15.1
-        asdf global postgres 15.1
-        asdf reshim postgres
-
-        echo 'pg_ctl() { "$HOME/.asdf/shims/pg_ctl" "$@"; }' >>~/.profile
-        source ~/.zshrc >/dev/null 2>&1
-        ;;
-    "Chrome")
-        sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-        sudo apt install -y ./google-chrome-stable_current_amd64.deb
-        ;;
-    "Node.js")
-        asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
-        asdf install nodejs 20.14.0
-        asdf global nodejs 20.14.0
-        asdf reshim nodejs 20.14.0
-        ;;
-    "ChromeDriver")
-        source ~/.zshrc >/dev/null 2>&1
-        npm install -g chromedriver
-        ;;
-    "Docker")
-        sudo apt-get update
-        sudo apt-get install -y \
-            ca-certificates \
-            curl \
-            gnupg \
-            lsb-release
-        sudo mkdir -p /etc/apt/keyrings
-        curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-        echo \
-            "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-            $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
-        sudo apt-get update
-        sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+        RUNLEVEL=1 sudo apt-get -y install postgresql
         ;;
     *)
         echo "Invalid name argument on install"
@@ -173,7 +88,7 @@ function maybe_install() {
         echo "$1 is already installed. Skipping..."
     else
         echo "Installing $1..."
-        if [[ $1 == "Homebrew" || $1 == "Erlang" ]]; then
+        if [[ $1 == "Erlang" ]]; then
             echo "This might take a while."
         fi
         echo ""
@@ -187,26 +102,13 @@ function add_env() {
     sleep 2
     maybe_install "Git"
 
-    echo ""
-    echo -e "${white}"
-    sleep 2
-    maybe_install "Zsh"
-
-    echo -e "${white}"
-    sleep 2
-    maybe_install "oh-my-zsh"
-
     echo -e "${white}"
     sleep 2
     maybe_install "wget"
 
     echo -e "${white}"
-    sleep 2
-    maybe_install "Homebrew"
-
-    echo -e "${white}"
     sleep 3
-    maybe_install "asdf"
+    maybe_install "mise"
 
     echo -e "${white}"
     sleep 1.5
@@ -223,24 +125,6 @@ function add_env() {
     echo -e "${white}"
     sleep 1.5
     maybe_install "PostgreSQL"
-
-    if [[ "$1" =~ ^([yY][eE][sS]|[yY])$ ]]; then
-        echo -e "${white}"
-        sleep 3
-        maybe_install "Chrome"
-        echo -e "${white}"
-
-        sleep 1.5
-        maybe_install "Node.js"
-        echo -e "${white}"
-
-        sleep 2
-        maybe_install "ChromeDriver"
-        echo -e "${white}"
-
-        maybe_install "Docker"
-        echo -e "${white}"
-    fi
 
     echo -e "${white}"
     echo -e "${cyan}${bold}phx.tools setup is complete!"
@@ -295,13 +179,11 @@ echo -e "${bblue}${bold}The following will be installed if not available already
 echo -e "${cyan}${bold}"
 
 echo "1) Build dependencies"
-echo "2) Zsh"
-echo "3) Homebrew"
-echo "4) asdf"
-echo "5) Erlang"
-echo "6) Elixir"
-echo "7) Phoenix"
-echo "8) PostgreSQL"
+echo "2) mise"
+echo "3) Erlang"
+echo "4) Elixir"
+echo "5) Phoenix"
+echo "6) PostgreSQL"
 
 echo ""
 echo -e "${white} ${bold}"
@@ -327,45 +209,9 @@ answer=''
 
 while ! is_yn "$answer"; do
     read -p "Do you want to continue? (y/n) " answer
-    echo ""
     case "$answer" in
     [yY] | [yY][eE][sS])
-        echo -e "${bblue}${bold}We can also install some optional tools:"
-
-        echo -e "${cyan}${bold}"
-
-        echo "1) Chrome"
-        echo "2) Node.js"
-        echo "3) ChromeDriver"
-        echo "4) Docker"
-
-        echo -e "${white}"
-        echo -e "${white} ${bold}"
-
-        optional=""
-
-        while ! is_yn "$optional"; do
-            read -p "Do you want us to install those as well? (y/n) " optional
-
-            if ! [[ "$optional" =~ ^([yY][eE][sS]|[yY]|[nN]|[nN][oO])$ ]]; then
-                echo "Please enter y or n"
-                echo ""
-            fi
-        done
-
-        echo ""
-
-        echo -e "${bblue}${bold}We're going to switch your default shell to Zsh even if it's not available yet, so you might see the following:"
-
-        echo -e "${bblue}${bold}chsh: Warning: /bin/zsh does not exist"
-
-        echo -e "${bblue}${bold}But don't worry. The installation will proceed as regular."
-
-        sleep 3
-
-        sudo -S chsh -s '/bin/zsh' "${USER}"
-
-        add_env "$optional"
+        add_env
         ;;
     [nN] | [nN][oO])
         echo "Thank you for your time"
--- priv/static/macOS.sh
+++ priv/static/macOS.sh
@@ -1,16 +1,6 @@
 #! /bin/bash
 
 # Make sure important variables exist if not already defined
-#
-# $USER is defined by login(1) which is not always executed (e.g. containers)
-# POSIX: https://pubs.opengroup.org/onlinepubs/009695299/utilities/id.html
-USER=${USER:-$(id -u -n)}
-# $HOME is defined at the time of login, but it could be unset. If it is unset,
-# a tilde by itself (~) will not be expanded to the current user's home directory.
-# POSIX: https://pubs.opengroup.org/onlinepubs/009696899/basedefs/xbd_chap08.html#tag_08_03
-HOME="${HOME:-$(getent passwd $USER 2>/dev/null | cut -d: -f6)}"
-# macOS does not have getent, but this works even if $HOME is unset
-HOME="${HOME:-$(eval echo ~$USER)}"
 
 bold=$(tput bold)
 normal=$(tput sgr0)
@@ -20,21 +10,26 @@ bblue='\033[1;34m'
 white='\033[0;37m'
 green='\033[0;32m'
 cyan='\033[0;36m'
+current_shell=$(basename "$SHELL")
+
+if [[ $current_shell == "zsh" ]]; then
+    config_file="/Users/$USER/.zshrc"
+else
+    current_shell="bash"
+    config_file="/Users/$USER/.bashrc"
+fi
 
 function already_installed() {
     case $1 in
-    "Xcode Command Line Tools")
+    "xcode")
         which xcode-select >/dev/null
         ;;
 
-    "oh-my-zsh")
-        [ -d ~/.oh-my-zsh ]
-        ;;
     "Homebrew")
         which brew >/dev/null 2>&1
         ;;
-    "asdf")
-        brew list | grep -q asdf
+    "mise")
+        which mise >/dev/null 2>&1
         ;;
     "Erlang")
         command -v erl >/dev/null 2>&1
@@ -45,20 +40,8 @@ function already_installed() {
     "Phoenix")
         mix phx.new --version >/dev/null 2>&1
         ;;
-    "Node.js")
-        which node >/dev/null 2>&1
-        ;;
     "PostgreSQL")
-        which pg_ctl >/dev/null 2>&1
-        ;;
-    "Chrome")
-        brew list | grep -q google-chrome
-        ;;
-    "ChromeDriver")
-        brew list | grep -q chromedriver
-        ;;
-    "Docker")
-        which docker >/dev/null 2>&1
+        which psql >/dev/null 2>&1
         ;;
     *)
         echo "Invalid name argument on checking"
@@ -68,77 +51,49 @@ function already_installed() {
 
 function install() {
     case $1 in
-    "Xcode Command Line Tools")
+    "xcode")
         xcode-select --install
-        ;;
-    "oh-my-zsh")
-        sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
         ;;
     "Homebrew")
         /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+        (
+            echo
+            echo 'eval "$(/opt/homebrew/bin/brew shellenv)"'
+        ) >>~/"$config_file"
+        eval "$(/opt/homebrew/bin/brew shellenv)"
         ;;
-    "asdf")
-        # Deps for asdf
-        brew install coreutils curl git
-
-        brew install asdf && echo -e "\n. $(brew --prefix asdf)/libexec/asdf.sh" >>${ZDOTDIR:-~}/.zshrc
+    "mise")
+        curl https://mise.run | sh
+        echo 'eval "$(~/.local/bin/mise activate $current_shell)"' >>~/"$config_file"
+        eval "$(~/.local/bin/mise activate $current_shell --shims)"
         ;;
     "Erlang")
         # Deps for erlang
         brew install autoconf openssl@1.1 wxwidgets libxslt fop
 
         export KERL_CONFIGURE_OPTIONS="--without-javac --with-ssl=$(brew --prefix openssl@1.1)"
-        asdf plugin add erlang https://github.com/asdf-vm/asdf-erlang.git
-        asdf install erlang 27.0
-        asdf global erlang 27.0
-        asdf reshim erlang 27.0
+        mise use -g erlang@27.0.1
         ;;
     "Elixir")
         # Deps for elixir
         brew install unzip
 
-        asdf plugin add elixir https://github.com/asdf-vm/asdf-elixir.git
-        asdf install elixir 1.17.1-otp-27
-        asdf global elixir 1.17.1-otp-27
-        asdf reshim elixir 1.17.1-otp-27
+        mise use -g elixir@1.17.2-otp-27
         ;;
     "Phoenix")
-        source ~/.zshrc >/dev/null 2>&1
         mix local.hex --force
         mix archive.install --force hex phx_new 1.7.0-rc.3
-        ;;
-    "Node.js")
-        asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
-        asdf install nodejs 20.14.0
-        asdf global nodejs 20.14.0
-        asdf reshim nodejs 20.14.0
         ;;
     "PostgreSQL")
         # Dependencies for PSQL
         brew install gcc readline zlib curl ossp-uuid
 
-        asdf plugin add postgres https://github.com/smashedtoatoms/asdf-postgres.git
-        asdf install postgres 15.1
-        asdf global postgres 15.1
-        asdf reshim postgres
-
-        echo 'pg_ctl() { "$HOME/.asdf/shims/pg_ctl" "$@"; }' >>~/.profile
-        source ~/.zshrc >/dev/null 2>&1
-        ;;
-    "Chrome")
-        brew install google-chrome
-        ;;
-    "ChromeDriver")
-        # Dependencies for chromedriver
-        brew install zip
-
-        asdf plugin add chromedriver
-        asdf install chromedriver latest
-        asdf global chromedriver latest
-        asdf reshim chromedriver latest
-        ;;
-    "Docker")
-        brew install --cask docker
+        brew install postgresql@16
+        (
+            export PATH="/opt/homebrew/opt/postgresql@16/bin:$PATH"
+            export LDFLAGS="-L/opt/homebrew/opt/postgresql@16/lib"
+            export CPPFLAGS="-I/opt/homebrew/opt/postgresql@16/include"
+        ) >>~/"$config_file"
         ;;
     *)
         echo "Invalid name argument on install"
@@ -167,15 +122,11 @@ function add_env() {
 
     echo -e "${white}"
     sleep 2
-    maybe_install "oh-my-zsh"
-
-    echo -e "${white}"
-    sleep 2
     maybe_install "Homebrew"
 
     echo -e "${white}"
     sleep 3
-    maybe_install "asdf"
+    maybe_install "mise"
 
     echo -e "${white}"
     sleep 1.5
@@ -192,24 +143,6 @@ function add_env() {
     echo -e "${white}"
     sleep 1.5
     maybe_install "PostgreSQL"
-
-    if [[ "$1" =~ ^([yY][eE][sS]|[yY])$ ]]; then
-        echo -e "${white}"
-        sleep 3
-        maybe_install "Chrome"
-        echo -e "${white}"
-
-        sleep 1.5
-        maybe_install "Node.js"
-        echo -e "${white}"
-
-        sleep 2
-        maybe_install "ChromeDriver"
-        echo -e "${white}"
-
-        maybe_install "Docker"
-        echo -e "${white}"
-    fi
 
     echo -e "${white}"
     echo -e "${cyan}${bold}phx.tools setup is complete!"
@@ -264,13 +197,12 @@ echo -e "${bblue}${bold}The following will be installed if not available already
 echo -e "${cyan}${bold}"
 
 echo "1) Build dependencies"
-echo "2) oh-my-zsh"
-echo "3) Homebrew"
-echo "4) asdf"
-echo "5) Erlang"
-echo "6) Elixir"
-echo "7) Phoenix"
-echo "8) PostgreSQL"
+echo "2) Homebrew"
+echo "3) mise"
+echo "4) Erlang"
+echo "5) Elixir"
+echo "6) Phoenix"
+echo "7) PostgreSQL"
 
 echo ""
 echo -e "${white} ${bold}"
@@ -296,45 +228,9 @@ answer=''
 
 while ! is_yn "$answer"; do
     read -p "Do you want to continue? (y/n) " answer
-    echo ""
     case "$answer" in
     [yY] | [yY][eE][sS])
-        echo -e "${bblue}${bold}We can also install some optional tools:"
-
-        echo -e "${cyan}${bold}"
-
-        echo "1) Chrome"
-        echo "2) Node.js"
-        echo "3) ChromeDriver"
-        echo "4) Docker"
-
-        echo -e "${white}"
-        echo -e "${white} ${bold}"
-
-        optional=""
-
-        while ! is_yn "$optional"; do
-            read -p "Do you want us to install those as well? (y/n) " optional
-
-            if ! [[ "$optional" =~ ^([yY][eE][sS]|[yY]|[nN]|[nN][oO])$ ]]; then
-                echo "Please enter y or n"
-                echo ""
-            fi
-        done
-
-        echo ""
-
-        echo -e "${bblue}${bold}We're going to switch your default shell to Zsh even if it's not available yet, so you might see the following:"
-
-        echo -e "${bblue}${bold}chsh: Warning: /bin/zsh does not exist"
-
-        echo -e "${bblue}${bold}But don't worry. The installation will proceed as regular."
-
-        sleep 3
-
-        sudo -S chsh -s '/bin/zsh' "${USER}"
-
-        add_env "$optional"
+        add_env
         ;;
     [nN] | [nN][oO])
         echo "Thank you for your time"
