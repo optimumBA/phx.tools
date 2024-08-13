508c0724d9ac9cf4418d8fdbbe5af0208cebaa5c
ngumokenneth <ngumokenneth97@gmail.com>
2024-08-13 14:07:12 +0300

    Update UI based on detected OS

--- lib/phx_tools_web/live/phx_tools_live/index.ex
+++ lib/phx_tools_web/live/phx_tools_live/index.ex
@@ -2,7 +2,6 @@ defmodule PhxToolsWeb.PhxToolsLive.Index do
   use PhxToolsWeb, :live_view
 
   alias PhxToolsWeb.Endpoint
-  alias PhxToolsWeb.InstructionComponents
   alias PhxToolsWeb.PhxToolsLive.LandingComponent
 
   @impl Phoenix.LiveView
@@ -12,5 +11,23 @@ defmodule PhxToolsWeb.PhxToolsLive.Index do
     do: assign(socket, :operating_system, operating_system)
 
   @impl Phoenix.LiveView
-  def handle_params(_params, _uri, socket), do: {:noreply, socket}
+  def handle_params(_params, _uri, socket) do
+    apply_action(socket, socket.assigns.live_action)
+  end
+
+  defp apply_action(socket, :index) do
+    case socket.assigns.operating_system do
+      "Linux" -> {:noreply, assign(socket, :live_action, :linux)}
+      "Mac" -> {:noreply, assign(socket, :live_action, :macOS)}
+      _other -> {:noreply, socket}
+    end
+  end
+
+  defp apply_action(socket, :linux) do
+    {:noreply, assign(socket, :live_action, :linux)}
+  end
+
+  defp apply_action(socket, :macOS) do
+    {:noreply, assign(socket, :live_action, :macOS)}
+  end
 end
--- lib/phx_tools_web/live/phx_tools_live/instruction_components.ex
+++ lib/phx_tools_web/live/phx_tools_live/instruction_components.ex
@@ -8,27 +8,22 @@ defmodule PhxToolsWeb.InstructionComponents do
   @type rendered :: Phoenix.LiveView.Rendered.t()
 
   slot :header, required: true
-  slot :installation_command, required: true
   slot :instruction, required: true
 
   @spec os_instructions(assigns()) :: rendered()
   def os_instructions(assigns) do
     ~H"""
-    <div class="font-normal solved-height">
-      <div class="flex items-center justify-center w-full h-full">
-        <div class="block lg:mt-[3%] ">
-          <h1 class="text-white text-center lg:text-2xl md:text-[22px] sm:text-xl lg:mb-[5%] md:mb-[2%] sm:mb-[2%]">
+    <div class="solved-height  bg-[#110A33] md:ml-24">
+      <div class="h-full shadow-[#C2B8FF33] shadow-md rounded-md lg:mx-[2%]">
+        <div class="text-start px-[3%] lg:text-xl md:text-lg sm:text-md sm:py-4">
+          <h1 class="text-white text-center lg:text-2xl md:text-[22px] sm:text-xl lg:my-[5%] sm:pb-3 ">
             <%= render_slot(@header) %>
           </h1>
-          <div class="h-full w-full border-4 shadow-[#28177E] shadow-md border-[#211666] rounded-md lg:mx-[2%] pb-2">
-            <div class="text-start px-[3%] lg:text-xl md:text-lg sm:text-md">
-              <%= for item <- @instruction do %>
-                <h1 class="text-white lg:mt-4 md:mt-5 sm:mt-4">
-                  <%= render_slot(item) %>
-                </h1>
-              <% end %>
-            </div>
-          </div>
+          <%= for item <- @instruction do %>
+            <h1 class="text-white ">
+              <%= render_slot(item) %>
+            </h1>
+          <% end %>
         </div>
       </div>
     </div>
