97a33330de076aa3104300bbae868626b6a82774
amos-kibet <kibetamos97@gmail.com>
2024-08-15 14:53:36 +0300

    Debug

--- priv/static/Linux.sh
+++ priv/static/Linux.sh
@@ -51,9 +51,12 @@ function install() {
         sudo apt-get install -y git
         ;;
     "mise")
-        curl https://mise.run | sh
-        echo 'eval "$(mise activate $current_shell)"' >>"$config_file"
-        eval "$($HOME/.local/bin/mise activate $current_shell --shims)"
+        apt update -y && apt install -y gpg sudo wget curl
+        sudo install -dm 755 /etc/apt/keyrings
+        wget -qO - https://mise.jdx.dev/gpg-key.pub | gpg --dearmor | sudo tee /etc/apt/keyrings/mise-archive-keyring.gpg 1>/dev/null
+        echo "deb [signed-by=/etc/apt/keyrings/mise-archive-keyring.gpg arch=amd64] https://mise.jdx.dev/deb stable main" | sudo tee /etc/apt/sources.list.d/mise.list
+        sudo apt update
+        sudo apt install -y mise
         ;;
     "Erlang")
         mise use -g erlang@27.0.1
