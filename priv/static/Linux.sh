9b1b922ca72d9c8fc1fe12570475cacf0ca5855d
amos-kibet <kibetamos97@gmail.com>
2024-08-16 17:47:52 +0300

    Debug

--- priv/static/Linux.sh
+++ priv/static/Linux.sh
@@ -1,6 +1,16 @@
 #! /bin/bash
 
 # Make sure important variables exist if not already defined
+#
+# $USER is defined by login(1) which is not always executed (e.g. containers)
+# POSIX: https://pubs.opengroup.org/onlinepubs/009695299/utilities/id.html
+USER=${USER:-$(id -u -n)}
+# $HOME is defined at the time of login, but it could be unset. If it is unset,
+# a tilde by itself (~) will not be expanded to the current user's home directory.
+# POSIX: https://pubs.opengroup.org/onlinepubs/009696899/basedefs/xbd_chap08.html#tag_08_03
+HOME="${HOME:-$(getent passwd $USER 2>/dev/null | cut -d: -f6)}"
+# macOS does not have getent, but this works even if $HOME is unset
+HOME="${HOME:-$(eval echo ~$USER)}"
 
 bold=$(tput bold)
 normal=$(tput sgr0)
@@ -10,19 +20,14 @@ bblue='\033[1;34m'
 white='\033[0;37m'
 green='\033[0;32m'
 cyan='\033[0;36m'
-current_shell=$(basename "$SHELL")
-
-if [[ $current_shell == "zsh" ]]; then
-    config_file="$HOME/.zshrc"
-else
-    current_shell="bash"
-    config_file="$HOME/.bashrc"
-fi
 
 function already_installed() {
     case $1 in
     "Git")
         which git >/dev/null 2>&1
+        ;;
+    "Zsh")
+        dpkg -l | grep -q zsh
         ;;
     "mise")
         which mise >/dev/null 2>&1
@@ -50,10 +55,13 @@ function install() {
     "Git")
         sudo apt-get install -y git
         ;;
+    "Zsh")
+        sudo apt-get install -y zsh
+        ;;
     "mise")
         curl https://mise.run | sh
-        echo 'eval "$(mise activate $current_shell)"' >>"$config_file"
-        eval "$(mise activate $current_shell --shims)"
+        echo 'eval "$(mise activate zsh)"' >>~/.zshrc
+        eval "$(mise activate zsh --shims)"
         ;;
     "Erlang")
         mise use -g erlang@27.0.1
@@ -95,6 +103,11 @@ function add_env() {
     echo -e "${white}"
     sleep 2
     maybe_install "Git"
+
+    echo ""
+    echo -e "${white}"
+    sleep 2
+    maybe_install "Zsh"
 
     echo -e "${white}"
     sleep 3
@@ -201,6 +214,19 @@ while ! is_yn "$answer"; do
     read -p "Do you want to continue? (y/n) " answer
     case "$answer" in
     [yY] | [yY][eE][sS])
+
+        echo ""
+
+        echo -e "${bblue}${bold}We're going to switch your default shell to Zsh even if it's not available yet, so you might see the following:"
+
+        echo -e "${bblue}${bold}chsh: Warning: /bin/zsh does not exist"
+
+        echo -e "${bblue}${bold}But don't worry. The installation will proceed as regular."
+
+        sleep 3
+
+        sudo -S chsh -s '/bin/zsh' "${USER}"
+
         add_env
         ;;
     [nN] | [nN][oO])
