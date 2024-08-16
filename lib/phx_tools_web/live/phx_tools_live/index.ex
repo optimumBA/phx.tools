80887240070c2baca866cf6a74d051eefa14eab4
Amos Kibet <kibetamos97@gmail.com>
2024-08-16 14:10:44 +0300

    Enable SEO (#18)

--- lib/phx_tools_web/live/phx_tools_live/index.ex
+++ lib/phx_tools_web/live/phx_tools_live/index.ex
@@ -6,11 +6,27 @@ defmodule PhxToolsWeb.PhxToolsLive.Index do
   alias PhxToolsWeb.PhxToolsLive.LandingComponent
 
   @impl Phoenix.LiveView
-  def mount(_params, session, socket), do: {:ok, assign_os(socket, session)}
+  def mount(_params, session, socket) do
+    {:ok,
+     socket
+     |> assign(:seo_attributes, %{})
+     |> assign_os(session)}
+  end
 
   defp assign_os(socket, %{"operating_system" => operating_system}),
     do: assign(socket, :operating_system, operating_system)
 
   @impl Phoenix.LiveView
-  def handle_params(_params, _uri, socket), do: {:noreply, socket}
+  def handle_params(_params, _uri, socket),
+    do: {:noreply, apply_action(socket, socket.assigns.live_action)}
+
+  defp apply_action(socket, :linux) do
+    assign(socket, seo_attributes: %{url: url(~p"/linux")})
+  end
+
+  defp apply_action(socket, :macOS) do
+    assign(socket, seo_attributes: %{url: url(~p"/macOS")})
+  end
+
+  defp apply_action(socket, _index), do: socket
 end
