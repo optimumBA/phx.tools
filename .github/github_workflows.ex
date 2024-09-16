dc3cc832e6417bd442c8be90673d4df2449e892c
Almir Sarajčić <almir.sarajcic@icloud.com>
2024-09-16 17:07:35 +0200

    Debug

--- .github/github_workflows.ex
+++ .github/github_workflows.ex
@@ -410,13 +410,13 @@ defmodule GithubWorkflows do
           name: "Test the script",
           if: "steps.result_cache.outputs.cache-hit != 'true'",
           run: "cd test/scripts && expect script.exp #{os}.sh",
-          shell: "zsh"
+          shell: "bash"
         ],
         [
           name: "Generate an app and start the server",
           if: "steps.result_cache.outputs.cache-hit != 'true'",
-          run: "source ~/.zshrc && make -f test/scripts/Makefile",
-          shell: "zsh"
+          run: "source ~/.bashrc && make -f test/scripts/Makefile",
+          shell: "bash"
         ],
         [
           name: "Check HTTP status code",
