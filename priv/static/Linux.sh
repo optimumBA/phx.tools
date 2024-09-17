9ce26d3f2a3cea8d565559fc45edd23cd5dd63d8
Almir Sarajčić <almir.sarajcic@icloud.com>
2024-09-17 19:19:14 +0200

    Debug

--- priv/static/Linux.sh
+++ priv/static/Linux.sh
@@ -137,30 +137,30 @@ function maybe_install() {
 }
 
 function add_env() {
-    echo ""
-    echo -e "${white}"
-    sleep 2
-    maybe_install "Git"
+    # echo ""
+    # echo -e "${white}"
+    # sleep 2
+    # maybe_install "Git"
 
-    echo -e "${white}"
-    sleep 2
-    maybe_install "wget"
+    # echo -e "${white}"
+    # sleep 2
+    # maybe_install "wget"
 
     echo -e "${white}"
     sleep 3
     maybe_install "mise"
 
-    echo -e "${white}"
-    sleep 1.5
-    maybe_install "Erlang"
+    # echo -e "${white}"
+    # sleep 1.5
+    # maybe_install "Erlang"
 
-    echo -e "${white}"
-    sleep 1.5
-    maybe_install "Elixir"
+    # echo -e "${white}"
+    # sleep 1.5
+    # maybe_install "Elixir"
 
-    echo -e "${white}"
-    sleep 1.5
-    maybe_install "Phoenix"
+    # echo -e "${white}"
+    # sleep 1.5
+    # maybe_install "Phoenix"
 
     echo -e "${white}"
     sleep 1.5
