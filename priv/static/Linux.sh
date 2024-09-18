d7d5445161babc0af2bc3ae11a7f59a79ba2021e
Almir Sarajčić <almir.sarajcic@icloud.com>
2024-09-19 00:30:01 +0200

    Try again

--- priv/static/Linux.sh
+++ priv/static/Linux.sh
@@ -84,11 +84,9 @@ install() {
         case $current_shell in
         "bash")
             echo 'eval "$(~/.local/bin/mise activate bash)"' >>$config_file
-            eval "$(~/.local/bin/mise activate bash)"
             ;;
         "zsh")
             echo 'eval "$(~/.local/bin/mise activate zsh)"' >>$config_file
-            eval "$(~/.local/bin/mise activate zsh)"
             ;;
         esac
         ;;
