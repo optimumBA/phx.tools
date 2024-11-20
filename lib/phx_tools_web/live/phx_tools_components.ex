a2c18a1e8a9692f33a89ad8a8517155b2a49bf90
mbashia <mbashiavictor@gmail.com>
2024-11-20 13:47:10 +0300

    Edit unsupported screen pop up

--- lib/phx_tools_web/live/phx_tools_components.ex
+++ lib/phx_tools_web/live/phx_tools_components.ex
@@ -36,60 +36,73 @@ defmodule PhxToolsWeb.PhxToolsComponents do
   @spec os_landing_card(assigns()) :: rendered()
   def os_landing_card(%{live_action: :index} = assigns) do
     ~H"""
-    <div class="flex flex-col items-center max-w-5xl mx-auto">
-      <div>
-        <div class="sm:flex flex-col items-center">
-          <Icons.phx_tools_svg class="h-7 md:h-12" />
-          <p class="sm:py-6 text-center text-white text-sm md:text-base lg:text-lg font-martian font-semibold ">
-            The Complete Development Environment for Elixir and Phoenix
-          </p>
-        </div>
-      </div>
-      <div class="flex flex-col items-center w-full md:w-11/12 lg:w-[1100px]">
-        <div class="w-[95%] md:w-[60%] flex">
-          <div class="flex justify-end items-center md:order-last md:mx-2 sm:mb-2">
-            <Heroicons.information_circle
-              class="hidden md:block w-6 h-6 text-white cursor-pointer focus:text-gray-500"
-              phx-click={JS.show(to: "#installation-instructions")}
-            />
-          </div>
-          <div class="border sm:px-3 bg-[#26168780] w-full flex items-center justify-center py-2 sm:space-x-3 rounded-sm overflow-x-auto whitespace-nowrap no-scrollbar">
-            <h1 id="tool-installation" class="scroll text-center text-white font-martian sm:text-sm">
-              <%= render_slot(@installation_command) %>
-            </h1>
-            <div id="copy" phx-hook="CopyHook" class="sm:py-3 md:my-0 cursor-pointer">
-              <Icons.copied_icon />
-              <Icons.copy_icon />
-            </div>
-          </div>
-        </div>
-
-        <div class="sm:grid  grid gap-4 sm:py-4">
-          <.source_code_button source_code_url={@source_code_url} />
-        </div>
-        <div class="-mt-6 md:mt-0">
-          <p class=" md:hidden text-center text-white font-martian text-xs md:text-sm lg:text-base leading-5 md:leading-6 sm:py-8">
-            <%= phx_tools_description() %>
-          </p>
-        </div>
-
-        <div class="bg-[#110A33] rounded-[4px] shadow-lg text-white shadow-[#2C2650] blur-shadow max-w-[809px]">
-          <div class="bg-[#2C2650] p-3 rounded-t-[4px] flex sm:flex-col md:flex-row sm:items-start md:items-center sm:space-y-2 md:space-y-0 md:space-x-4 sm:justify-start md:justify-center">
-            <Icons.exclamation_icon />
-            <p class="font-martian sm:text-sm md:text-base lg:text-lg md:py-2">
-              Unsupported Operating System Detected
+    <div class="solved-height">
+      <div class="min-h-full">
+        <div class="mt-[5%] md:flex flex-col items-center max-w-4xl mx-auto">
+          <div class="sm:flex flex-col items-center">
+            <Icons.phx_tools_svg class="h-7 md:h-12" />
+            <p class="sm:py-6 text-center text-white text-sm md:text-base lg:text-lg font-martian font-semibold ">
+              The Complete Development Environment for Elixir and Phoenix
             </p>
           </div>
-          <p class="text-xs leading-5 md:leading-6 md:text-sm lg:text-base text-center sm:px-3 font-martian sm:py-6 md:px-8">
-            It looks like you're using an operating system that Phx.tools doesn't currently support. This script is designed to work on Linux and macOS only. Please switch to a compatible operating system to continue.
-          </p>
+
+          <div class="md:flex lg:w-[75%] flex-col relative">
+            <div class="md:flex">
+              <div class="flex justify-end items-center md:order-last md:mx-2 sm:mb-2">
+                <Heroicons.information_circle
+                  class="hidden md:block w-6 h-6 text-white cursor-pointer focus:text-gray-500"
+                  phx-click={JS.show(to: "#installation-instructions")}
+                />
+              </div>
+              <div class="border sm:px-3 bg-[#26168780] w-full flex items-center justify-center py-2 sm:space-x-3 rounded-sm overflow-x-auto whitespace-nowrap no-scrollbar">
+                <h1 id="tool-installation" class="text-center text-white font-martian sm:text-sm">
+                  <%= render_slot(@installation_command) %>
+                </h1>
+                <div id="copy" phx-hook="CopyHook" class="sm:py-3 md:my-0 cursor-pointer">
+                  <Icons.copied_icon />
+                  <Icons.copy_icon />
+                </div>
+              </div>
+            </div>
+            <div class="w-full bg-blue-600 flex justify-center">
+              <.os_instructions
+                :let={installation_instruction}
+                installation_instructions={installation_instructions(@live_action)}
+                live_action={@live_action}
+                operating_system={@operating_system}
+              >
+                <%= raw(installation_instruction) %>
+              </.os_instructions>
+            </div>
+
+            <div class="  flex justify-center items-center gap-4 sm:py-4">
+              <.source_code_button source_code_url={@source_code_url} />
+            </div>
+            <div class="-mt-6 md:mt-0">
+              <p class=" md:hidden text-center text-white font-martian text-xs md:text-sm lg:text-base leading-5 md:leading-6 sm:py-8">
+                <%= phx_tools_description() %>
+              </p>
+            </div>
+
+            <div class="bg-[#110A33] rounded-[4px] shadow-lg text-white shadow-[#2C2650] blur-shadow max-w-[809px]">
+              <div class="bg-[#2C2650] p-3 rounded-t-[4px] flex sm:flex-col md:flex-row sm:items-start md:items-center sm:space-y-2 md:space-y-0 md:space-x-4 sm:justify-start md:justify-center">
+                <Icons.exclamation_icon />
+                <p class="font-martian sm:text-sm md:text-base lg:text-lg md:py-2">
+                  Unsupported Operating System Detected
+                </p>
+              </div>
+              <p class="text-xs leading-5 md:leading-6 md:text-sm lg:text-base text-center sm:px-3 font-martian sm:py-6 md:px-8">
+                It looks like you're using an operating system that Phx.tools doesn't currently support. This script is designed to work on Linux and macOS only. Please switch to a compatible operating system to continue.
+              </p>
+            </div>
+            <p class="hidden md:block text-center text-white font-martian text-xs md:text-sm lg:text-base leading-5 md:leading-6 sm:py-8">
+              <%= phx_tools_description() %>
+            </p>
+          </div>
+          <.web_updates />
+          <.footer />
         </div>
-        <p class="hidden md:block text-center text-white font-martian text-xs md:text-sm lg:text-base leading-5 md:leading-6 sm:py-8">
-          <%= phx_tools_description() %>
-        </p>
       </div>
-      <.web_updates />
-      <.footer />
     </div>
     """
   end
@@ -210,6 +223,15 @@ defmodule PhxToolsWeb.PhxToolsComponents do
       "Type \"Terminal\" and hit <b>RETURN</b>",
       "Paste the shell command by hitting <b>⌘ + V</b> together",
       "Run the command by hitting <b>RETURN</b>"
+    ]
+  end
+
+  defp installation_instructions(_) do
+    [
+      "Click on the copy icon to copy this command to your clipboard",
+      "Open Terminal by pressing <b class=\"font-extrabold\">Ctrl + Alt + T</b> together",
+      "Paste the shell command by pressing <b>Shift + Ctrl + V</b> together",
+      "Run the command by hitting <b>ENTER</b>"
     ]
   end
 
