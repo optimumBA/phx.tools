e43c6be2ae1769d1d29037393725c12b7d3d6924
mbashia <mbashiavictor@gmail.com>
2024-11-20 11:00:25 +0300

    Edit rounded corners of unsupported os frame

--- lib/phx_tools_web/live/phx_tools_components.ex
+++ lib/phx_tools_web/live/phx_tools_components.ex
@@ -73,8 +73,8 @@ defmodule PhxToolsWeb.PhxToolsComponents do
           </p>
         </div>
 
-        <div class=" bg-[#110A33] rounded-[4px] shadow-lg text-white shadow-[#2C2650] blur-shadow max-w-[809px]">
-          <div class="bg-[#2C2650] p-3 rounded-t-xl flex sm:flex-col md:flex-row sm:items-start md:items-center sm:space-y-2 md:space-y-0 md:space-x-4 sm:justify-start md:justify-center">
+        <div class="bg-[#110A33] rounded-[4px] shadow-lg text-white shadow-[#2C2650] blur-shadow max-w-[809px]">
+          <div class="bg-[#2C2650] p-3 rounded-t-[4px] flex sm:flex-col md:flex-row sm:items-start md:items-center sm:space-y-2 md:space-y-0 md:space-x-4 sm:justify-start md:justify-center">
             <Icons.exclamation_icon />
             <p class="font-martian sm:text-sm md:text-base lg:text-lg md:py-2">
               Unsupported Operating System Detected
